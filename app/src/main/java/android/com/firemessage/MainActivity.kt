8625fff5f28bddd89a4a0fa834cde16d118e5d25
jeremyschee <schee01329@gmail.com>
2019-06-16 21:41:52 +0800

    end of tut02

--- app/src/main/java/android/com/firemessage/MainActivity.kt
+++ app/src/main/java/android/com/firemessage/MainActivity.kt
@@ -1,7 +1,10 @@
 package android.com.firemessage
 
+import android.annotation.SuppressLint
+import android.com.firemessage.fragment.MyAccountFragment
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,10 +21,19 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
                 R.id.navigation_my_account ->{
+                    replaceFragment(MyAccountFragment())
                     true
                 }
                 else -> false
             }
+        }
+    }
+
+    @SuppressLint("CommitTransaction")
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_layout,fragment)
+            commit()
         }
     }
 }
--- app/src/main/java/android/com/firemessage/fragment/MyAccountFragment.kt
+++ app/src/main/java/android/com/firemessage/fragment/MyAccountFragment.kt
@@ -1,6 +1,5 @@
 package android.com.firemessage.fragment
 
-
 import android.app.Activity
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -9,10 +8,20 @@ import android.view.View
 import android.view.ViewGroup
 
 import android.com.firemessage.R
+import android.com.firemessage.SignInActivity
+import android.com.firemessage.util.FirestoreUtil
+import android.com.firemessage.util.StorageUtil
 import android.content.Intent
 import android.graphics.Bitmap
 import android.provider.MediaStore
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.storage.FirebaseStorage
+import android.com.firemessage.glide.GlideApp
+import kotlinx.android.synthetic.main.fragment_my_account.*
 import kotlinx.android.synthetic.main.fragment_my_account.view.*
+import org.jetbrains.anko.clearTask
+import org.jetbrains.anko.newTask
+import org.jetbrains.anko.support.v4.intentFor
 import java.io.ByteArrayOutputStream
 
 class MyAccountFragment : Fragment() {
@@ -29,11 +38,25 @@ class MyAccountFragment : Fragment() {
                 val intent = Intent().apply {
                     type = "image/"
                     action = Intent.ACTION_GET_CONTENT
-                    putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/jpg","image/png"))
+                    putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/jpg", "image/png"))
                 }
-                startActivityForResult(Intent.createChooser(intent,"Select Image"),RC_SELECT_IMAGE)
+                startActivityForResult(Intent.createChooser(intent, "Select Image"), RC_SELECT_IMAGE)
             }
-        }
+                btn_save.setOnClickListener {
+                    if (::selectedImageBytes.isInitialized)
+                        StorageUtil.uploadProfilePhoto(selectedImageBytes){ imagePath ->
+                            FirestoreUtil.updateCurrentUser(editText_name.text.toString(),editText_bio.text.toString(),imagePath)
+                        }
+                    else
+                        FirestoreUtil.updateCurrentUser(editText_name.text.toString(),editText_bio.text.toString(),null)
+                }
+                btn_sign_out.setOnClickListener {
+                    AuthUI.getInstance().signOut(this@MyAccountFragment.context!!)
+                        .addOnCompleteListener {
+                            startActivity(intentFor<SignInActivity>().newTask().clearTask())
+                        }
+                }
+            }
         return view
     }
 
@@ -44,10 +67,23 @@ class MyAccountFragment : Fragment() {
             val outputStream = ByteArrayOutputStream()
             selectedImageBmp.compress(Bitmap.CompressFormat.JPEG,90,outputStream)
             selectedImageBytes = outputStream.toByteArray()
+
             //TODO: Load picture
+            GlideApp.with(this).load(selectedImageBytes).into(imageView_profile_picture)
+
             pictureJustChanged = true
         }
     }
 
-
+    override fun onStart() {
+        super.onStart()
+        FirestoreUtil.getCurrentUser { user ->
+            if (this@MyAccountFragment.isVisible){
+                editText_name.setText(user.name)
+                editText_bio.setText(user.bio)
+                if (!pictureJustChanged && user.profilePicturePath != null)
+                       GlideApp.with(this).load(StorageUtil.pathToReference(user.profilePicturePath)).placeholder(R.drawable.ic_account_circle_black_24dp).into(imageView_profile_picture)
+            }
+        }
+    }
 }
